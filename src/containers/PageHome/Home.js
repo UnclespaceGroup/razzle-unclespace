2ad7d99665fbdde77900249c089f68bbedd6a160
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2020-07-25 18:43:55 +0300

    add: add fonts

--- src/containers/PageHome/Home.js
+++ src/containers/PageHome/Home.js
@@ -16,24 +16,7 @@ function Home() {
 
   return (
     <div className="Home">
-      <div className="Home-header">
-        <h2>Welcome to Razzle</h2>
-      </div>
-      <p className="Home-intro">
-        To get started, edit <code>src/App.js</code> or{' '}
-        <code>src/Home.js</code> and save to reload.
-      </p>
-      <ul className="Home-resources">
-        <li>
-          <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-        </li>
-        <li>
-          <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-        </li>
-        <li>
-          <a href="https://palmer.chat">Community Slack</a>
-        </li>
-      </ul>
+      <h2>Сборка Unclespace</h2>
     </div>
   )
 }
